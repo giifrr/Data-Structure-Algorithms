7fedb49ee0c1e45ad024f40825b26a2f293a7cac
Gifar Al Anshar <gifar.anshar12@gmail.com>
2023-01-02 21:58:26 +0700

    adds recursion problems

--- Leetcode/g.rb
+++ Leetcode/g.rb
@@ -1,28 +1,4 @@
-def two_out_of_three(nums1, nums2, nums3)
-  temp = Array.new(101, 0)
+def first_uniq_char(s)
+  s = s.to_a.uniq
 
-  for value in nums1.uniq do
-    temp[value] += 1
-  end
-
-  for value in nums2.uniq do
-    temp[value] += 1
-  end
-
-  for value in nums3.uniq do
-    temp[value] += 1
-  end
-
-  res = []
-
-  for i in (0...101) do
-    if temp[i] > 1
-      res << i
-    end
-  end
-
-  res
 end
-
-nums1 = [1,1,3,2]; nums2 = [2,3]; nums3 = [3]
-puts two_out_of_three(nums1, nums2, nums3)
